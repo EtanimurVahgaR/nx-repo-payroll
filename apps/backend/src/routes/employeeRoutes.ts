1530e92e1e4591a75a3032ecf841239c7c45376c
Raghav Aloria <raghavaloria@gmail.com>
2025-07-05 17:47:47 +0530

    authroization confirming

--- apps/backend/src/routes/employeeRoutes.ts
+++ apps/backend/src/routes/employeeRoutes.ts
@@ -13,7 +13,7 @@ route.post('/add-test-user', add_test_user);
 
 route.use(authenticateJWT);
 route.use(authorizeByDesignation);
-route.get('/all', authenticateJWT, get_all_employees);
+route.get('/all', get_all_employees);
 route.post('/add-new', add_new_employee);
 
 route.post('/:employeeCode/markAttendance', mark_attendance);
